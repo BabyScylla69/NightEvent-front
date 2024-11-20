13d7049929349f7a708f9a62628df29833bb1e5c
Jordi <jordiigarciatorres@gmail.com>
2024-11-20 19:36:51 +0100

    first commit

--- src/app/Router.jsx
+++ src/app/Router.jsx
@@ -1,5 +1,4 @@
 import { BrowserRouter, Route, Routes} from 'react-router-dom';
-import Register from '../pages/Register';
 import Events from '../pages/Events'
 import Event from '../pages/Event'
 import CreateEvent from '../pages/CreateEvent'
@@ -9,7 +8,6 @@ const Router = () => (
   <BrowserRouter>
     <Routes>
       <Route index element={<Events/>} />
-      <Route path="/register" element={<Register/>} />
       <Route path="/event:id" element={<Event/>} />
       <Route path="/create-event" element={<CreateEvent/>} />
       <Route path="/profile:id" element={<Profile/>}></Route>
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -4,11 +4,22 @@ import { useUserContext } from "../providers/UserProvider";
 
 const Login = () => {
    const {setUser} = useUserContext();
+
+   const [u, setU] = useState();
+   const [popUp, setPopUp] = useState(false);
+
    const handleClick = async () => {
       const response = await login(u)
       setUser(response)
    }
-  const [u, setU] = useState();
+
+   const openPopUp = () => {
+      setPopUp(true)
+   }
+
+   const closePopup = () => {
+    setPopUp(false);
+  };
 
   return (
     <div>
@@ -17,6 +28,21 @@ const Login = () => {
 
       <button onClick={handleClick}>Enviar</button> 
       <br />
+      <button onClick={openPopUp}>Register</button>
+
+      {popUp && (
+        <div className="popup">
+          <div className="popup-content">
+            <span className="close" onClick={closePopup}>
+              &times;
+            </span>
+            <h2>Register</h2>
+            <input type="text" placeholder="username" onChange={(e) => setU({...u, username: e.target.value})} />
+            <input type="password" placeholder="password" onChange={(e) => setU({...u, password: e.target.value})} />
+            <button onClick={handleClick}>Enviar</button>
+          </div>
+        </div>
+      )}
     </div>
   );
 };
